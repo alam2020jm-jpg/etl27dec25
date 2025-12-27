74312d442eaa033cddf34df35a88af3637ab3905
zaffir <alam2020jm@gmail.com>
2025-12-27 11:31:34 +0530

    updated

--- cfunction.py
+++ cfunction.py
@@ -9,19 +9,19 @@ def my_function(cloud_event):
     region="us-central1"
     main_pyspark_code="gs://mainpysparkjob"
     input_path=f"gs://{bucket}/{file_name}"
-    gcs_output=f"gs://processed27dec25"
+    gcs_output=f"gs://processed27dec25/output"
     bq_table="b27dec25.t27dec25.et27dec25"
-    date_of_processed=current_date().strftime("%Y%m%d%h%m%s")
-    job_id=f"etl_job_id-{date_of_processed}"
-    api_end=f"{region}/googleapi.dataproc:443"
-    client=dataproc_v1.BsatchControllerClient(client_options=(apiend_point:api_end))
+    date_of_processed=datetime.now().strftime("%Y%m%d-%H%M%S")
+    job_id=f"etl-job-id-{date_of_processed}"
+    api_end=f"{region}-dataproc.googleapis.com:443"
+    client=dataproc_v1.BatchControllerClient(client_options={"api_endpoint":api_end})
     job_args=[input_path,gcs_output,bq_table]
-    pyspark_batch=dataproc_v1.PySparkBatch("main_python_file_uri"=main_pyspark_code\
-                  args=job_args)
+    pyspark_batch=dataproc_v1.PySparkBatch(main_python_file_uri=main_pyspark_code\
+                  ,args=job_args)
     job_config=dataproc_v1.Batch(pyspark_batch=pyspark_batch)
     operation=client.create_batch(
-                                parents=f"projects/{project_id}/locations/{region}",
-                                batch=job_config,job=job_id)
+                                parent=f"projects/{project_id}/locations/{region}",
+                                batch=job_config,batch_id=job_id)
     
 
 
