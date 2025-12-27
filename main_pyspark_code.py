6c85b3d51fb728f701b93ddd2d31dd6a5a3ccb51
zaffir <alam2020jm@gmail.com>
2025-12-27 10:33:13 +0530

    mainPysparkCodeUpdated

--- main_pyspark_code.py
+++ main_pyspark_code.py
@@ -1,12 +1,12 @@
-from pyspar.sql import SparkSession
+from pyspark.sql import SparkSession
 import sys
 spark=SparkSession\
      .builder.appName("Myetl27dec25")\
      .config("spark.sql.dynamicAllaocation.autoBroadcastJoinThreshold","50MB")\
      .getOrCreate()
-input_data_path=sys.argv[0]
-gcs-Out_path=sys.argv[1]
-bq_out_table=sys.argv[2]
+input_data_path=sys.argv[1]
+gcs_Out_path=sys.argv[2]
+bq_out_table=sys.argv[3]
 df=spark.read.format("csv")\
   .option('mode','PERMISSIVE')\
   .option("columnNameofCorruptRecord","CorrutedRecords")\
@@ -14,5 +14,13 @@ df=spark.read.format("csv")\
   .option("inferSchema",True)\
   .load(input_data_path)
 df.printSchema()
- 
-  .
+df.write.format("csv")\
+  .option('mode',"append")\
+  .option("header",True)\
+  .save(gcs_Out_path)
+df.write.format("bigquery")\
+  .option('table',bq_out_table)\
+  .option("TemporaryGCSBucket","gs://tembqpath")\
+  .option('mode',"append")\
+  .save()
+spark.stop()
